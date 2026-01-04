9d1294d535205e5ec64cd3c5df3d9b7dd75d8d21
Paul Thrasher <thrashr888@gmail.com>
2026-01-04 03:22:29 -0800

    Update rdos to 0.4.0 (ARM64 only)

--- Formula/rdos.rb
+++ Formula/rdos.rb
@@ -1,26 +1,18 @@
 class Rdos < Formula
   desc "R-DOS: A modern TUI file manager inspired by Q-DOS II, written in Rust"
   homepage "https://github.com/thrashr888/QDOS"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/thrashr888/QDOS/releases/download/v0.3.0/rdos-macos-aarch64"
-      sha256 "97d5386398b9cd9ef274df3d362069d316643c541c6b9bfe242dad0c598ec9a2"
-    end
-    on_intel do
-      url "https://github.com/thrashr888/QDOS/releases/download/v0.3.0/rdos-macos-x86_64"
-      sha256 "10e7e0ff5d3ab6ea6de2e835d9ebcd51a0f2f8e781bc31538bbc2b81a3f6b68f"
+      url "https://github.com/thrashr888/QDOS/releases/download/v0.4.0/rdos-macos-aarch64"
+      sha256 "e4a8cadf027efa94b7d53d86eff4313b1f3ad6a9ee1228a7287b314e450b8a65"
     end
   end
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "rdos-macos-aarch64" => "rdos"
-    else
-      bin.install "rdos-macos-x86_64" => "rdos"
-    end
+    bin.install "rdos-macos-aarch64" => "rdos"
   end
 
   test do
