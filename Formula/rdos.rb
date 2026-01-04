e3ac5023cfaba26a8a5a259bb96c4299ee104395
Paul Thrasher <thrashr888@gmail.com>
2026-01-03 20:25:31 -0800

    Update to v0.3.0

--- Formula/rdos.rb
+++ Formula/rdos.rb
@@ -1,17 +1,17 @@
 class Rdos < Formula
   desc "R-DOS: A modern TUI file manager inspired by Q-DOS II, written in Rust"
   homepage "https://github.com/thrashr888/QDOS"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/thrashr888/QDOS/releases/download/v0.2.0/rdos-macos-aarch64"
-      sha256 "448f90751584fece20f148e5340feb1f3a3a644c941aa8dca9fff614eab27c98"
+      url "https://github.com/thrashr888/QDOS/releases/download/v0.3.0/rdos-macos-aarch64"
+      sha256 "97d5386398b9cd9ef274df3d362069d316643c541c6b9bfe242dad0c598ec9a2"
     end
     on_intel do
-      url "https://github.com/thrashr888/QDOS/releases/download/v0.2.0/rdos-macos-x86_64"
-      sha256 "3837d5c1bba444e87e579f80d1533a2369a51e0dfb7c0059409c2936c5883816"
+      url "https://github.com/thrashr888/QDOS/releases/download/v0.3.0/rdos-macos-x86_64"
+      sha256 "10e7e0ff5d3ab6ea6de2e835d9ebcd51a0f2f8e781bc31538bbc2b81a3f6b68f"
     end
   end
 
