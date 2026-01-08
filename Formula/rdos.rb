a50b01283c4bd18acd9fb0916461321e951b7329
Paul Thrasher <thrashr888@gmail.com>
2026-01-07 16:56:01 -0800

    Update rdos to v0.8.0

--- Formula/rdos.rb
+++ Formula/rdos.rb
@@ -1,13 +1,13 @@
 class Rdos < Formula
   desc "R-DOS: A modern TUI file manager inspired by Q-DOS II, written in Rust"
   homepage "https://github.com/thrashr888/QDOS"
-  version "0.7.0"
+  version "0.8.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/thrashr888/QDOS/releases/download/v0.7.0/rdos-macos-aarch64"
-      sha256 "8127b6c93c98e0dd207321326386d2989d88865bfc4b837ef01b71a4b3cc459c"
+      url "https://github.com/thrashr888/QDOS/releases/download/v0.8.0/rdos-macos-aarch64"
+      sha256 "6abd722e3c769db74967c4f62ec6d08c24f073a90369f2f36aa8b7f8e1e1b4af"
 
       def install
         bin.install "rdos-macos-aarch64" => "rdos"
@@ -15,8 +15,8 @@ class Rdos < Formula
     end
 
     on_intel do
-      url "https://github.com/thrashr888/QDOS/releases/download/v0.7.0/rdos-macos-x86_64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/thrashr888/QDOS/releases/download/v0.8.0/rdos-macos-x86_64"
+      sha256 "dd34248ddedeafcd3303e91c590101eb283fdda9da3d59e32a7505dd078dcae5"
 
       def install
         bin.install "rdos-macos-x86_64" => "rdos"
