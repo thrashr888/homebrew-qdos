984706993e7c8351e683788f9527adf06ed22cfe
Paul Thrasher <thrashr888@gmail.com>
2026-01-14 23:23:45 -0800

    Update rdos to v0.16.0

--- Formula/rdos.rb
+++ Formula/rdos.rb
@@ -1,13 +1,13 @@
 class Rdos < Formula
   desc "R-DOS: A modern TUI file manager inspired by Q-DOS II, written in Rust"
   homepage "https://github.com/thrashr888/QDOS"
-  version "0.15.0"
+  version "0.16.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/thrashr888/QDOS/releases/download/v0.15.0/rdos-macos-aarch64"
-      sha256 "ba48a8ee49ce525ecfac8bdb0a17395123cef5e2508dbebceb6268eac005b597"
+      url "https://github.com/thrashr888/QDOS/releases/download/v0.16.0/rdos-macos-aarch64"
+      sha256 "c610254f7606aad0401fa3843f4854d6f21bd5740a680ad34cefca8803db9a3f"
 
       def install
         bin.install "rdos-macos-aarch64" => "rdos"
